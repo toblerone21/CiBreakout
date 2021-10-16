6e3efac7d79aabcea3843d14e8fe5583f03be932
Matthew Harris <Matt25373@gmail.com>
2021-10-16 13:56:40 -0400

    first commit

---
+++ Public/Assets/js/app.js
@@ -0,0 +1,21 @@
+var MyApp = (function(){
+
+    function init (uid, mid){
+        
+        event_process_for_signaling_server();
+    }
+    var socket = null;
+    function event_process_for_signaling_server(){
+        socket = io.connect();
+        socket.on("connect", () => {
+            alert("socket connected to client side");
+        });
+    }
+
+
+    return{
+        _init: function(uid, mid){
+            init(uid, mid);
+        },
+    };
+})();
